ee74b2bf6280cc76da494706516cd247ba2ae455
ItsRezaMosavi <ItsRezaMosavi@gmail.com>
2026-05-30 13:29:55 +0330

    feat: add core application service interfaces
    - IClock: abstracts UTC time for testability
    - ICurrentUser<TId>: exposes auth state, user ID, and roles
    - IDomainEventDispatcher: dispatches domain events asynchronously

---
+++ src/BuildingBlocks.Application/ICurrentUser.cs
@@ -0,0 +1,8 @@
+﻿namespace BuildingBlocks.Application;
+
+public interface ICurrentUser<out TId>
+{
+    bool IsAuthenticated { get; }
+    TId? UserId { get; }
+    IReadOnlyCollection<string> Roles { get; }
+}
---
+++ src/BuildingBlocks.Application/IDomainEventDispatcher.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlocks.Domain.Events;
+
+namespace BuildingBlocks.Application;
+
+public interface IDomainEventDispatcher
+{
+    Task DispatchAsync(IEnumerable<IDomainEvent> events, CancellationToken cancellationToken = default);
+}
